bd1b0ea867d4af5e583f78c63b672222a81676ff
taty2010 <shydalys123@gmail.com>
2022-12-14 19:21:32 -0600

    added success page

---
+++ src/success.tsx
@@ -0,0 +1,12 @@
+export default function Success() {
+  return (
+    <div className="container">
+
+      <main>
+        <h1>Submission Received!</h1>
+        <p>Thank you for your feedback! Head back to the <a href="/">home page</a>.</p>
+      </main>
+
+    </div>
+  )
+}
