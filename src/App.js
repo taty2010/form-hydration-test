b8860d439480cb21a425d27d2367ce736e719ab4
taty2010 <shydalys123@gmail.com>
2022-12-15 19:36:48 -0600

    added success link for testing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './App.css';
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Link} from "react-router-dom";
 import Success from "./success";
 import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
+      <Link to="/success">Success</Link>
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/success" element={<Success/>} />
