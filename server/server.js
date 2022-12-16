22d52c6de0462c01bf21ccb5ea1b9df72216c856
taty2010 <shydalys123@gmail.com>
2022-12-15 19:34:38 -0600

    update staticRoute

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use("^/$", (req, res, next) => {
     return res.send(
       data.replace(
         '<div id="root"></div>',
-        `<div id="root">${ReactDOMServer.renderToString(<StaticRouter><App /></StaticRouter>)}</div>`
+        `<div id="root">${ReactDOMServer.renderToString(<StaticRouter location={req.url}><App /></StaticRouter>)}</div>`
       )
     );
   });
